6d40768fed6a2ed80c2900a6502f5f007e461db2
congtongc <lmy000524@gmail.com>
2022-06-07 23:42:25 +0900

    화면3,4 구성 완료 -수면 추천 부분 제외 완료(133번줄)

--- Best_sleep.py
+++ Best_sleep.py
@@ -30,11 +30,12 @@ class Main(f):
     def __init__(self, master):
         f.__init__(self, master)
         #폰트
-        global D_font, T_font, S_font, I_font
+        global D_font, T_font, S_font, I_font,BT_font
         D_font = tk.font.Font(family="나눔고딕", size=20)
         T_font = tk.font.Font(family="나눔고딕", size=30, weight="bold")
         S_font = tk.font.Font(family="나눔고딕", size=15)
         I_font = tk.font.Font(family="나눔고딕", size=17)
+        BT_font = tk.font.Font(family="나눔고딕",size=40,weight="bold")
         #타이틀
         tk.Label(self, text="최적의 수면시간 찾기", pady=150, font=T_font).pack()
         #수면 데이터 입력하기 버튼
@@ -47,6 +48,7 @@ class Main(f):
                     command=lambda: master.g_switch_frame(S_recommend)).pack()
         #경고
         tk.Label(self, text="*반드시 데이터를 먼저 입력하세요.", fg='red', font=S_font).pack()
+        
 
 class S_data(f):
     def __init__(self, master):
@@ -84,16 +86,54 @@ class S_data(f):
         tk.Button(self, text="Reset", font=D_font, width=7, command=I_reset).grid(column = 5, row = 8, columnspan=2)
 
 class S_recommend(f):
+  
     def __init__(self, master):
         f.__init__(self, master)
+        global sleepH, sleepM
+        sleepH = tk.StringVar(master)
+        sleepM = tk.StringVar(master)
         #뒤로가기 버튼
         tk.Button(self, text="뒤로", font=D_font,
-                    command=lambda: master.p_switch_frame(Main)).grid(column = 1, row = 0, padx = 30, pady = 30)
+                    command=lambda: master.p_switch_frame(Main)).grid(column = 0, row = 0, padx = 20, pady = 30)
         #Blank
-        tk.Label(self).grid(column = 0, row = 0, pady = 50, padx = 20)
+        tk.Label(self, text = "").grid(column = 0, row = 0)
         #타이틀
-        tk.Label(self, text="최적의 수면시간 찾기", font=T_font).grid(column = 2, row = 2, padx = 140)
-        tk.Label(self, text="최적의 수면시간 추천 받기", font=D_font).grid(column = 2, row = 3)
+        tk.Label(self, text="최적의 수면시간 찾기", font=T_font).grid(column = 2, row = 1,columnspan=20,padx=200)
+        tk.Label(self, text="최적의 수면시간 추천 받기", font=D_font).grid(column =2, row = 2,columnspan=20)
+        #기상 시간
+        tk.Label(self, text="기상 시간 입력: ", font=D_font).grid(column=9, row=4,pady=100)
+        S_hour = tk.Entry(self, borderwidth = 3, font=I_font, width=7,textvariable=sleepH)
+        S_hour.grid(column = 10, row = 4)
+        tk.Label(self, text="시", font=D_font).grid(column = 11, row = 4)
+        S_minute = tk.Entry(self, borderwidth = 3, font=I_font, width=7,textvariable=sleepM)
+        S_minute.grid(column = 12, row = 4)
+        tk.Label(self, text=" 분", font=D_font).grid(column = 13, row = 4)
+        #입력 버튼
+        tk.Button(self, text="입력", font=D_font, width=7,
+                    command=lambda: master.g_switch_frame(S_final)).grid(column = 9, row = 5,columnspan=3)
+                    
+        #리셋 버튼
+        def I_reset():
+            S_hour.delete(0, len(S_hour.get()))
+            S_minute.delete(0, len(S_minute.get()))
+        tk.Button(self, text="Reset", font=D_font, width=7, command=I_reset).grid(column = 10, row = 5, columnspan=3)
+
+class S_final(f):
+    def __init__(self, master):
+        f.__init__(self, master)
+        print("d",sleepH.get(),sleepM.get())
+        #blank
+        tk.Label(self, text="").grid(column = 0, row = 0,padx=150,pady=100)
+        #입력한 기상 시간 출력
+        tk.Label(self, text="입력한 기상 시간은", font=D_font).grid(column=0, row=1,columnspan=3)
+        tk.Label(self, text=f"{sleepH.get()}시 {sleepM.get()}분", font=T_font).grid(column=1,row=1,sticky="e")
+        tk.Label(self, text="이고", font=D_font).grid(column=2,row=1,sticky="w")
+        #최적의 수면 시작 시간 출력
+        tk.Label(self, text="최적의 수면 시작 시간은", font=D_font).grid(column=1, row=2,ipadx=30)
+        tk.Label(self, text="00시 57분 ",font=BT_font).grid(column=1,row=3,sticky="w")
+        tk.Label(self, text="입니다.", font=D_font).grid(column=1, row=3,sticky="e")
+        #확인 버튼
+        tk.Button(self, text="확인", font=D_font, width=7,command=lambda: master.p_switch_frame(Main)).grid(column = 1, row = 4,pady=20)
 
 if __name__ == "__main__":
     win = tool()
