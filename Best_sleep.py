63cf185761a40d5e3a28daaefb7861a29b717c1e
che0432 <cho0432@gmail.com>
2022-06-12 17:54:44 +0900

    구현완료

--- Best_sleep.py
+++ Best_sleep.py
@@ -2,7 +2,8 @@ from tkinter import *
 import tkinter as tk
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import Sleepalgorithm
+import newSleepalgorithm
+import numpy as np
 
 f = tk.Frame
 
@@ -81,8 +82,8 @@ class S_data(f):
         S_safac.grid(column = 4, row = 6, ipady=10)
         tk.Label(self, text="*1 ~ 10 사이로 입력해주세요.", fg='red', font=S_font).grid(column = 5, row = 6, columnspan=3)
         def S_data_save():
-            Sleepalgorithm.answerTime=(S_hour.get()*60)+S_minute.get()
-            Sleepalgorithm.answerSatisfy=S_safac.get()
+            newSleepalgorithm.answerTime=(S_hour.get()*60)+S_minute.get()
+            newSleepalgorithm.answerSatisfy=S_safac.get()
             tk.Label(self, text=f"{S_hour.get()}시간 {S_minute.get()}분 만족도 {S_safac.get()}점 저장되었습니다.", font=D_font).grid(column = 3, row = 9, columnspan=5)
             S_hour.delete(0, len(S_hour.get()))
             S_minute.delete(0, len(S_minute.get()))
@@ -99,8 +100,8 @@ class S_recommend(f):
     def __init__(self, master):
         f.__init__(self, master)
         global sleepH, sleepM
-        sleepH = tk.StringVar(master)
-        sleepM = tk.StringVar(master)
+        sleepH = tk.IntVar(master)
+        sleepM = tk.IntVar(master)
         #뒤로가기 버튼
         tk.Button(self, text="뒤로", font=D_font,
                     command=lambda: master.p_switch_frame(Main)).grid(column = 0, row = 0, padx = 20, pady = 30)
@@ -112,10 +113,12 @@ class S_recommend(f):
         #기상 시간
         tk.Label(self, text="기상 시간 입력: ", font=D_font).grid(column=9, row=4,pady=100)
         S_hour = tk.Entry(self, borderwidth = 3, font=I_font, width=7,textvariable=sleepH)
+        S_hour.delete(0, len(S_hour.get()))
         S_hour.focus()
         S_hour.grid(column = 10, row = 4)
         tk.Label(self, text="시", font=D_font).grid(column = 11, row = 4)
         S_minute = tk.Entry(self, borderwidth = 3, font=I_font, width=7,textvariable=sleepM)
+        S_minute.delete(0, len(S_minute.get()))
         S_minute.grid(column = 12, row = 4)
         tk.Label(self, text=" 분", font=D_font).grid(column = 13, row = 4)
         #입력 버튼
@@ -135,7 +138,7 @@ class S_final(f):
         tk.Label(self, text="").grid(column = 0, row = 0,padx=100,pady=3)
         tk.Label(self, text="").grid(column = 0, row = 2, pady=1)
         #그래프
-        df = Sleepalgorithm.df
+        df = newSleepalgorithm.df
         figure2 = plt.figure(figsize=(6,4))
         ax2= figure2.add_subplot(111)
         line2 = FigureCanvasTkAgg(figure2, self)
@@ -147,9 +150,19 @@ class S_final(f):
         tk.Label(self, text="입력한 기상 시간은", font=D_font).grid(column=0, row=3,columnspan=3)
         tk.Label(self, text=f"{sleepH.get()}시 {sleepM.get()}분", font=T_font).grid(column=2,row=3,sticky="e")
         tk.Label(self, text="이고", font=D_font).grid(column=3,row=3,sticky="w")
+        #추천시간 계산
+        S_rec_max = newSleepalgorithm.S_rec_max
+        S_rec = ((sleepH.get()*60)+sleepM.get())-(S_rec_max)
+        if S_rec>=0:
+            S_rec_H = int (S_rec/60)
+            S_rec_M = S_rec - (S_rec_H*60)
+        else:
+            S_rec += 1440
+            S_rec_H = int (S_rec/60)
+            S_rec_M = S_rec - (S_rec_H*60)
         #최적의 수면 시작 시간 출력
         tk.Label(self, text="최적의 수면 시작 시간은", font=D_font).grid(column=2, row=4,ipadx=30)
-        tk.Label(self, text="00시 57분 ",font=BT_font).grid(column=2,row=5,sticky="w")
+        tk.Label(self, text=f"{S_rec_H}시 {S_rec_M}분 ",font=BT_font).grid(column=2,row=5,sticky="w")
         tk.Label(self, text="입니다.", font=D_font).grid(column=2, row=5,sticky="e")
         #확인 버튼
         tk.Button(self, text="확인", font=D_font, width=7,command=lambda: master.p_switch_frame(Main)).grid(column = 2, row = 6,pady=20)
